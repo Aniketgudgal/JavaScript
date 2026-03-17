269c8992d3a5ec072a1b6adc498081711899e412
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-17 14:42:53 +0530

    variables in javascript

---
+++ Variables/VarDeclaration.js
@@ -0,0 +1,22 @@
+let a = 123
+console.log(typeof(a))
+console.log(a)
+let b = 'a'
+console.log(typeof(b))
+console.log(b)
+let c = "Aniket"
+console.log(typeof(c))
+console.log(c)
+let d = 12.5
+console.log(typeof(d))
+console.log(d)
+let e = true
+console.log(typeof(e))
+console.log(e)
+let f = 1234n;
+console.log(typeof(f))
+console.log(f)
+
+let ab = 16 + "aniket";
+console.log(typeof(ab))
+console.log(ab)

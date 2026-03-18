0ffacc0036b439a7f81a000dda435f874aa59bd9
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-18 17:25:00 +0530

    print all even number from 1 to 100

---
+++ Loop/PrintEvenNum.js
@@ -0,0 +1,11 @@
+//Q4. Write a java program to print all even numbers between 1 to 100.- using while loop
+let num = 100;
+let i = 1;
+while(i <= num)
+{
+	if(i % 2 == 0)
+	{
+		console.log(i);
+	}
+	i++;
+}

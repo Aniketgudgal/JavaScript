06578d5ba773ab58ceba7114cf085acdb2a9f37a
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-18 17:48:54 +0530

    Print the Table

---
+++ Loop/PrintTable.js
@@ -0,0 +1,10 @@
+//Q9. Write a java program to print a multiplication table of any number.
+
+let num = 87;
+let i = 1;
+console.log("Table of "+num + " is following");
+while(i <= 10)
+{
+	console.log(i*num);
+	i++;
+}

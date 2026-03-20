b5d3fc419eae0ba826a7dc0cdf183d8d3d17935f
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-21 00:04:11 +0530

    changes

---
+++ Variables/UserInput.js
@@ -0,0 +1,19 @@
+//let num = prompt("Enter the Number");
+//console.log(num);
+
+function showData()
+{
+	alert("Hello world");
+}
+function downAciton()
+{
+	alert("Down event applyed");
+}
+function printtext()
+{
+	alert("orgnization");
+}
+function outMo()
+{
+	alert("Heading Details");
+}
---
+++ basic calculator/index.js
@@ -0,0 +1,37 @@
+function show()
+{
+    let ans = [
+        ['1','2','3','+'],
+        ['4','5','6','-'],
+        ['7','8','9','*'],
+        ['0','.','/','=']
+    ];
+    let table = document.getElementById("tbdata");
+    table.style.margin="auto";
+    let textInfo = document.getElementById("showInfo");
+    let info = document.createElement("input");
+    info.setAttribute("type","text");
+    info.setAttribute("value","");
+    info.style.width= "100%";
+    info.style.height = "50px";
+    textInfo.appendChild(info);
+    for(let i = 0; i < ans.length; i++)
+    {
+        let trbtn = document.createElement("tr");
+        for(let j = 0; j < ans[i].length;j++)
+        {
+            let bnt = document.createElement("td");
+            let inp = document.createElement("input");
+            inp.setAttribute("type","button");
+            inp.setAttribute("value",""+ans[i][j]);
+            inp.style.width="100px";
+            inp.style.fontSize= "1.5em";
+            inp.style.textAlign="center";
+            inp.style.height="100px";
+            bnt.appendChild(inp);
+            trbtn.append(bnt);
+        }
+        table.appendChild(trbtn);
+    }
+
+}

ef9001a592f808b3e733214c9bf7fba1d96cc5ab
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-25 21:50:26 +0530

    Change Background Color using DOM

---
+++ DOM/BackgrounColorEvent/background.js
@@ -0,0 +1,18 @@
+
+function showData()
+{
+    let text = document.getElementById("pchange");
+    text.innerHTML = "THANK YOU FOR SHWOING CHANGES";
+    text.style.alignItems = "center";
+    text.style.backgroundColor = "highlight";
+    text.style.color = "black";
+    text.style.padding = "8px";
+}
+function showInfo()
+{
+    let s = document.getElementById("pchange");
+    s.innerHTML = "Check Information";
+    s.style.color = "red";
+    s.style.backgroundColor = "white";
+    s.style.padding = "0px";
+}

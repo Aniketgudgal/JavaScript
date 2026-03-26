b329643e74bc676b6c4557b31669823b947fe63b
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-26 19:48:37 +0530

    Changing the Font size using DOM

---
+++ DOM/FontSize/index.js
@@ -0,0 +1,15 @@
+let a = document.getElementById("ptext");
+a.innerHTML = "Hello World In This Programming";
+let val = 1.2;
+a.style.fontSize = val+"em";
+document.getElementById();
+function increase()
+{
+    ++val;
+    a.style.fontSize = val+"em";
+}
+function decrease()
+{
+    --val;
+    a.style.fontSize = val+"em";
+}

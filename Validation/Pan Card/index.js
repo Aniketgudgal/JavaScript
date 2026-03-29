7b2acb8b010d81ad075317a8e2e4cf13ed44d0da
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-29 18:21:58 +0530

    Pan Number Validation

---
+++ Validation/Pan Card/index.js
@@ -0,0 +1,66 @@
+function checkData()
+{
+    let dv = document.getElementById("access");
+    let val = dv.value;
+    let msg = document.getElementById("msg");
+    let spn = document.createElement("span");
+    spn.style.marginBottom = "15px";
+    let lname = document.getElementById("sname").value;
+    lname = lname[0];
+    console.log(lname);
+    if(val.length == 10)
+    {
+        let arr = new Array(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1);
+        for(let i = 0; i < val.length; i++)
+        {
+            if(val.charAt(i) >= 'A' && val.charAt(i) <= 'Z' && i < 3)
+            {
+                arr[i] = 1;
+            }
+            else if(val.charAt(i) >= 'A' && val.charAt(i) <= 'Z' && i == 3 && val.charAt(i) == lname)
+            {
+                arr[i] = 1;
+            }
+            else if(val.charAt(i) >= '0' && val.charAt(i) <= '9' && i <= 8)
+            {
+                arr[i] = 1;
+            }
+            else if(val.charAt(i) >= 'A' && val.charAt(i) <= 'Z' && (i == val.length - 1))
+            {
+                console.log(val.charAt(i));
+                if(val.charAt(i) == lname)
+                {
+                    arr[i] = 1;
+                }   
+            }
+        }
+        let sum = 0;
+        for(let i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+    
+        if(sum == arr.length)
+        {
+            msg.innerHTML = "";
+            spn.innerText = "Valid Pan Number";
+            spn.style.color = "green";
+            msg.appendChild(spn);
+        }
+        else
+        {
+            msg.innerHTML = "";
+            spn.innerText = "Invalid Number";
+            spn.style.color = "red";
+            msg.appendChild(spn);
+        }
+    }
+    else
+    {
+        msg.innerHTML = "";
+        spn.innerText = "Invalid Number";
+        spn.style.color = "red";
+        msg.appendChild(spn);
+    }
+    
+}

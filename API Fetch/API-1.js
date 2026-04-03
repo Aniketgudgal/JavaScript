f51d82b26aecd183225ad4e49b16e6b96f276640
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-03 12:52:14 +0530

    Fetch API and show into console window

---
+++ API Fetch/API-1.js
@@ -0,0 +1,13 @@
+let promise = fetch("https://jsonplaceholder.typicode.com/posts");
+let a = promise.then((val) =>{ 
+let js = val.json();
+js.then((data) => 
+{
+	for(let i = 0; i < 5; i++)
+	{
+		console.log(data[i].userId+"\t"+data[i].id+"\t"+data[i].title);
+	}
+}
+).catch((irr) => console.log("Inner Error"+irr));
+}).catch((erro) => console.log("Error is: "+erro));
+

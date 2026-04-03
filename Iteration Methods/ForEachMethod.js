dcd01748c760cfb37fff9ef95a3d81d1cc0b8072
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-03 14:43:43 +0530

    Operation on data

--- Iteration Methods/ForEachMethod.js
+++ Iteration Methods/ForEachMethod.js
@@ -1,3 +1,3 @@
 let a = [10,20,30,40,50];
-a.forEach((val)=> console.log(val));
+a.forEach((val)=> console.log(value));
 
---
+++ Iteration Methods/MapMethod.js
@@ -0,0 +1,5 @@
+let a  = [10,11,12,13,14,15,16,17];
+let newArr = a.map((val) =>{
+	return val*2;
+});
+console.log(newArr);

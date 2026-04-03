e5696a8145463d63056dc1d0a9cb92ba7a778ec8
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-03 15:18:45 +0530

    Object in JS

--- Object/ObjJs.js
+++ Object/ObjJs.js
@@ -9,3 +9,4 @@ console.log("ID of Person is: "+objData.id);
 console.log("Name of person is: "+objData.name);
 console.log("address of person is: "+objData.address);
 console.log("Contact Number is: "+objData.contactNum);
+console.log(typeof(objData));

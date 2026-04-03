a1f24543518c8b4011f0ba31fb5a4dee40447c4e
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-03 15:18:12 +0530

    Object in JS

---
+++ Object/ObjJs.js
@@ -0,0 +1,11 @@
+let objData = {
+	id: 1,
+	name: "aniket",
+	address: "Ahilyanagar",
+	contactNum: 7057112026
+};
+console.log(objData);
+console.log("ID of Person is: "+objData.id);
+console.log("Name of person is: "+objData.name);
+console.log("address of person is: "+objData.address);
+console.log("Contact Number is: "+objData.contactNum);

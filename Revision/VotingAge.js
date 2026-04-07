61e7f9d4a084206ef2ef2107104b37a30b41af2d
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-07 12:48:26 +0530

    Check the Voter is Eligibile for vote

---
+++ Revision/VotingAge.js
@@ -0,0 +1,17 @@
+/*
+Voting Eligibility
+Logic:
+If age ≥ 18 → eligible
+Example:
+Input: 17
+Output: Not eligible
+*/
+let age = 18;
+if(age < 18)
+{
+	console.log("Not eligible");
+}
+else 
+{
+	console.log("eligible");
+}

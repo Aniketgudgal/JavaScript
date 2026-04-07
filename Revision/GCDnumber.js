012e737633333afb375cd8b4cc02fa0763580a53
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-07 15:47:42 +0530

    Calculate the GCD of Number

---
+++ Revision/GCDnumber.js
@@ -0,0 +1,22 @@
+/*
+GCD of Two Numbers
+Logic:
+Find largest number dividing both numbers.
+Example:
+Input: 12, 18
+Output: 6
+*/
+let num1 = 12;
+let num2 = 18;
+let max = 0;
+for(let i = 1; i <= Math.min(num1,num2); i++)
+{
+	if(num1 % i == 0 && num2 % i == 0)
+	{
+		if(i > max)
+		{
+			max = i;
+		}
+	}
+}
+console.log("GCD of: "+num1+", "+num2+" is: "+max);

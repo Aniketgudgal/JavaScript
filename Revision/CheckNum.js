47f136088bf71624aba30c9b3d27a68678a7e93f
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-07 12:22:07 +0530

    Check number is positive or negative or equals to zero

---
+++ Revision/CheckNum.js
@@ -0,0 +1,21 @@
+/*
+1. Positive, Negative, or Zero
+Logic:
+Check if number > 0, < 0, or equal to 0.
+Example:
+Input: -5
+Output: Negative number
+*/
+let a = -1;
+if(a > 0)
+{
+	console.log("Positive Number");
+}
+else if(a == 0)
+{
+	console.log("Number is zero");
+}
+else
+{
+	console.log("Negative Number");
+}

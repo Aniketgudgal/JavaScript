344b7f4acb064381907b14497f0a3d5e052ba792
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-07 15:04:30 +0530

    print table of given number

---
+++ Revision/TablePrint.js
@@ -0,0 +1,14 @@
+/*
+Multiplication Table
+Logic:
+Multiply number with 1–10.
+Example:
+Input: 3
+Output: 3, 6, 9 ... 30
+18. Count Digits
+*/
+let num = 3;
+for(let i = 1; i <= 10; i++)
+{
+	console.log(num*i);
+}

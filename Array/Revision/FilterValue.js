7eda91823c6a8a0d89a622904397678d7561835f
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-08 15:18:25 +0530

    Filter the data using Filter function

---
+++ Array/Revision/FilterValue.js
@@ -0,0 +1,7 @@
+/*
+Q2. Filter Even Numbers
+Problem: Filter even numbers from [1,2,3,4,5,6] using .filter().
+*/
+let a = [1,2,3,4,5,6];
+let fArray = a.filter((val) => val % 2 == 0);
+console.log(fArray);

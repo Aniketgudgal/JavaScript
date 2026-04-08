9c591045529d2d2e238884dd6012bff41a657c88
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-08 15:44:35 +0530

    Remove Element in array using shift method

---
+++ Array/Revision/RemoveFirstElement.js
@@ -0,0 +1,8 @@
+/*
+ Remove First Element
+Problem: Remove first element of [10,20,30] using .shift().
+*/
+let a = [10,20,30];
+let result = a.shift();
+console.log("Removed element is: "+result);
+console.log(a);
